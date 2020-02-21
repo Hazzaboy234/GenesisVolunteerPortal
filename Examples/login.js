0163946c589ec9d1feeae68c4f27eb5f0e4ab668
Samuel Sogbesan <ss3222@bath.ac.uk>
2020-02-21 22:48:09 +0000

    Update Validation Module; Separate Front-end and Back-end validation responsibilities;;

--- Examples/login.js
+++ Examples/login.js
@@ -1,25 +1,32 @@
-var button = document.getElementById("login-button");
-button.addEventListener("click",validate);
-
+/*
+    Function that checks if input credentials exist in DB
+    returns boolean
+*/
+function combinationExists(email,password){
+    //Do ajax request to back-end to find out if the email and password exist in the database
+    return false;
+}
 
 function validate(){
     var email = document.getElementById("email").value;
     var pass = document.getElementById("password").value;
 
-    var validator = new RegExp("\w+@\w+[.]\w+");
-    var isEmailValid=validator.test(email);
-    var isPasswordValid=true;
-
-    console.log(isEmailValid);
-    console.log(isPasswordValid);
-
-    if(isEmailValid===true && isPasswordValid===true){
-        //Do cheeky request
-        
-    }else{
-        alert("Invalid info fool");
-    }
-
+    /*
+    Front-end validation
+    ensure email and password match the sign-up criteria now
+    because if they don't they definately can't be a valid entry in the database
+    so there is no need to go and check
+    */
+    var isEmailValid=RegExp("(\\w+@\\w+[.]\\w+)").test(email);
+    var isPasswordValid=RegExp("^.*(?=.{8,})(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*").test(pass);
 
     
+    if(((isEmailValid===true && isPasswordValid===true))&&(combinationExists(email,pass)===true)){
+        //insert cookie information
+        alert("Information valid!");
+        //redirect to next window
+    }else{
+        alert("Invalid info \nEmail:"+email+"\npass:"+pass);
+    }    
 }
+document.getElementById("login-button").addEventListener("click",validate);
