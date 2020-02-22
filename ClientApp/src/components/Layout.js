1fdf30fb89eeae09af6c668634072829b575e4e1
Samuel Sogbesan <ss3222@bath.ac.uk>
2020-02-22 13:50:15 +0000

    Create React Components for Header and Footer
    Added React Components for the Header and the Footer

--- ClientApp/src/components/Layout.js
+++ ClientApp/src/components/Layout.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import { Container } from 'reactstrap';
 import { NavMenu } from './NavMenu';
-
+import {NavigationBar} from "./NavigationBar";
+import "./NavigationBar.css";
+import { Footer } from './Footer';
 export class Layout extends Component {
   static displayName = Layout.name;
 
   render () {
     return (
       <div>
-        <NavMenu />
+        <NavigationBar/>
         <Container>
           {this.props.children}
         </Container>
+        <Footer/>
       </div>
     );
   }
---
+++ ClientApp/src/components/NavigationBar.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+export class NavigationBar extends Component {
+    render() {
+        return (
+            <div class="header-container">
+                <div class="header">
+                    <img id="header-logo" alt="Genesis Trust" src={require("./Resources/logo.png")}/>
+                <nav id="profile-nav">
+                        <ul>
+                            <li>
+                                <img
+                                    alt="profile"
+                                    src={require("./placeholder-icon.png")}
+                                    width="30px"
+                                />
+                            </li>
+                            <li><a>Profile</a></li>
+                            <li><a>Settings</a></li>
+                            <li><a>Help</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <nav id="main-nav">
+                    <ul>
+                        <li class="main-nav-item active"><a href="/">Home</a></li>
+                        <li class="main-nav-item"><a>Roles</a></li>
+                        <li class="main-nav-item"><a>Projects</a></li>
+                        <li class="main-nav-item"><a>Policies</a></li>
+                    </ul>
+                </nav>
+            </div>
+        )
+    }
+}
