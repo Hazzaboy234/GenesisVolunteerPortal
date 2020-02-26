03df7e3cab84024fd6957365c87ec5599360a78f
Harry <hbll20@bath.ac.uk>
2020-02-26 22:11:51 +0000

    Login

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,6 +1,9 @@
 using System.Threading.Tasks;
+using GenesisVolunteerPortal.Logic;
 using GenesisVolunteerPortal.Logic.Database;
 using GenesisVolunteerPortal.Models;
+using GenesisVolunteerPortal.Logic.Database.DatabaseModels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GenesisVolunteerPortal.Controllers
@@ -10,15 +13,33 @@ namespace GenesisVolunteerPortal.Controllers
     public class LoginController : Controller
     {
         private readonly IDatabase _database;
+        private readonly IAuthorisationManager _authorisationManager;
 
-        public LoginController(IDatabase database)
+        public LoginController(IDatabase database, IAuthorisationManager authorisationManager)
         {
             _database = database;
+            _authorisationManager = authorisationManager;
         }
 
         public async Task<ActionResult> Login(LoginAttempt attempt)
         {
+            var response = await _authorisationManager.AuthoriseLogin(attempt);
+            if(!response.Verified)
+            {
+                return Unauthorized(response.ErrorMessage);
+            }
+            return Ok(response.Token);
+        }
+
+        public async Task<ActionResult> Register(Persons attempt)
+        {
             return null;
+        }
+
+        [HttpGet("/login/{id}")]
+        public string GetPassword(string password)
+        {
+            return _authorisationManager.GeneratePass(password);
         }
     }
 }
