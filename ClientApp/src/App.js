8a798932a7c3e07bbc071bb9ce8772ef088307fd
mt923 <48826728+mt923@users.noreply.github.com>
2020-03-10 11:41:05 +0000

    Settings Component

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -11,6 +11,7 @@ import { Dashboard } from './components/Dashboard';
 import { Login } from './components/Login';
 import { Signup } from './components/Signup';
 import { Boilerplate } from './components/Boilerplate';
+import { Settings } from './components/Settings';
 
 export default class App extends Component {
   static displayName = App.name;
@@ -23,6 +24,7 @@ export default class App extends Component {
         <Route path='/login' component={Login} />
         <Route path='/signup' component={Signup} />
         <Route path='/dogs' component={Boilerplate} />
+        <Route path='/settings' component={Settings} />
       </Layout>
     );
   }
