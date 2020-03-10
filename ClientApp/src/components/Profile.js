e290935a948199888cce0348fbe0b17e04d78359
Lottie Carr <44607114+lottieisabelle@users.noreply.github.com>
2020-03-10 10:55:50 +0000

    Create Profile.js file

---
+++ ClientApp/src/components/Profile.js
@@ -0,0 +1,12 @@
+import React,{Component} from "react";
+
+export class Profile extends Component{
+    render(){
+        return(
+            <div>
+                <link rel="stylesheet" href={require("./profile.css")}/>
+                Content here
+            </div>
+        )
+    }
+}
