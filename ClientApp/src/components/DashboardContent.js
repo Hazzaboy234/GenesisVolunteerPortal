91b2edca29d2595d6b4e3e97e5deead5a2605710
Samuel Sogbesan <ss3222@bath.ac.uk>
2020-03-12 14:47:07 +0000

    Update DashboardContent.js

--- ClientApp/src/components/DashboardContent.js
+++ ClientApp/src/components/DashboardContent.js
@@ -1,36 +1,36 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { Map } from "./Map";
-export class DashboardContent extends Component{
-    render(){
-        return(
+export class DashboardContent extends Component {
+    render() {
+        return (
             <div className="main-container">
-            <div className="map-container">
-                <nav id="map-nav">
-                    <ul>
-                        <li className="map-nav-item active"><a href="./">Map</a></li>
-                        <li className="map-nav-item"><a href="./">Calendar</a></li>
-                        <li className="map-nav-item new-content-alert"><a href="./">Noticeboard</a></li>
-                    </ul>
-                </nav>  
-                <div className="map">
-                    <Map />
-                </div>
-            </div>
-
-            <div className="secondary-container">
-                <div className="noti-container">
-                    <span>Notifications</span>
-                    <div>
+                <div className="map-container">
+                    <nav id="map-nav">
                         <ul>
-                            <li>Confirm Email Address</li>
-                            <li>Complete Application</li>
-                            <li>LMAO</li>
+                            <li className="map-nav-item active"><a href="./">Map</a></li>
+                            <li className="map-nav-item"><a href="./">Calendar</a></li>
+                            <li className="map-nav-item new-content-alert"><a href="./">Noticeboard</a></li>
                         </ul>
+                    </nav>
+                    <div className="map">
+                        <Map />
+                    </div>
+                </div>
 
+                <div className="secondary-container">
+                    <div className="noti-container">
+                        <span>Notifications</span>
+                        <div>
+                            <ul>
+                                <li>Confirm Email Address</li>
+                                <li>Complete Application</li>
+                                <li>LMAO</li>
+                            </ul>
+
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>            
         )
     }
 }
